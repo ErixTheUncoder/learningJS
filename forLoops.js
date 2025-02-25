b74266ca6fa770521e78acb4b1a82ee397b7de62
ErixTheUncoder <erichong.0969@gmail.com>
2025-02-25 14:50:50 +0800

    Added forLoops.js and whileLoops.js

---
+++ forLoops.js
@@ -0,0 +1,11 @@
+let numbers = [1,2,3,4,5,6,7,8];
+
+let index = 0;
+let lengthArray = numbers.length;
+
+console.log(numbers[index]);
+console.log(lengthArray);
+
+for(;index<lengthArray;index++){
+  console.log(numbers[index]);
+}
---
+++ whileLoops.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
+let index = 0;
+
+while(index<numbers.length){
+  console.log(numbers[index]);
+  index++;
+}
