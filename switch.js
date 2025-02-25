65e4d7ba84e63bedd32e03766ea927ed88e326f9
ErixTheUncoder <erichong.0969@gmail.com>
2025-02-25 14:43:45 +0800

    Added switch.js

---
+++ switch.js
@@ -0,0 +1,13 @@
+let job = null;
+
+switch(job){
+  case "Saleman":
+    console.log("Direct selling");
+    break;
+  case "Software Developer":
+    console.log("Writes code");
+    break;
+  default:
+    console.log("Jobless + L + ratio");
+    break;
+}
