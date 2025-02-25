7c9cab1e530761e3aec4f5ec4db1df9d3e2b4688
ErixTheUncoder <erichong.0969@gmail.com>
2025-02-25 15:06:35 +0800

    Added forOfLoop.js

---
+++ forOfLoop.js
@@ -0,0 +1,9 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
+
+// for(let index=0;index<lengthArray;index++){
+//   console.log(numbers[index]);
+// }
+
+for(let element of numbers){
+  console.log(numbers[element]);
+}
