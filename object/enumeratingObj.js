9e0e38caccacd0bab5b64ac995ccee3ce4f5b0a6
ErixTheUncoder <erichong.0969@gmail.com>
2025-03-10 15:03:56 +0800

    Added enumrate file

---
+++ object/enumeratingObj.js
@@ -0,0 +1,17 @@
+const dog = {
+    name:'Max',
+    age:5,
+    eyeColor:'blue'
+};
+
+//[1,2,3,4]
+//['Max',5,'blue']
+//['Max','John','Reed']
+
+for (const key in dog){
+    console.log(dog[key]);
+}
+
+const keys = 
+
+/// 
